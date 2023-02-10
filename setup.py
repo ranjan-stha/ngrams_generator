91235fb26908d12f3893898c4bbcf00c4607c3ec
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2023-02-10 11:47:56 +0545

    added the setup file for package creation

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ngrams_generator",
+    author="rsh",
+    author_email="",
+    description="Generates the n-grams from the texts",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    # Dependencies
+    install_requires=[
+        "nltk>=3.8.1",
+        "langdetect==1.0.9"
+    ],
+    version="0.1",
+    license="MIT",
+    long_description=open("README.md").read(),
+)
